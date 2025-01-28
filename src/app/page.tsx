81e39f0b8a11dfeab12317f006d616be452e4a5d
da-nn-yy <db7394928@gmail.com>
2025-01-28 19:38:01 +0300

    reordered sections in main tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,22 @@ import Navbar from '@/components/NavBar';
 export default function Home() {
   return (
     <main className="bg-[#0a192f] text-white">
-      <Navbar />
-      <section id="hero">
-        <Hero />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="projects">
-        <Projects />
-      </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
+          <Navbar />
+          <section id="hero">
+            <Hero />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          <section id="skills">
+            <Skills />
+          </section>
+          <section id="projects">
+            <Projects />
+          </section>
+          <section id="contact">
+            <Contact />
+          </section>
     </main>
   );
 }
